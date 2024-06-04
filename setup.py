602a718ba6603025a453aa17dbffa4ac1f50488a
faith6 <root39293@gmail.com>
2024-06-05 03:48:00 +0900

    chore: Update dependencies in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='bombay',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -16,6 +16,7 @@ setup(
         'chromadb',
         'termcolor',
         'colorama',
+        'pyfiglet'
     ],
     author='faith6',
     author_email='root39293@gmail.com',
